868db5cd1267384adcf564a5d6db25ec838e4661
thrillim <86657389+thrillim@users.noreply.github.com>
2023-02-22 01:00:52 +0700

    complete sjf_n and reorder code

--- CPU-Scheduling/script.js
+++ CPU-Scheduling/script.js
@@ -10,12 +10,12 @@ class Process {
     }
 }
 
-// Define the processes
+// Define the processes in order of arrival time
 const processes = [
     new Process("P1", 0, 4, 1),
-    new Process("P2", 5, 3, 2),
-    new Process("P3", 6, 5, 3),
-    new Process("P4", 15, 4, 4)
+    new Process("P2", 0, 1, 2),
+    new Process("P3", 1, 2, 3),
+    new Process("P4", 2, 1, 4)
 ];
 
 const unit = 50; // width of a cell
@@ -29,7 +29,7 @@ const algorithms = {
     RR: "Round Robin"
 }
 
-const selectedAlg = "FCFS";
+const selectedAlg = "SJF_N";
 const heading = document.getElementById("algorithm");
 
 let totalWaitingTime = 0;
@@ -67,43 +67,62 @@ if (selectedAlg === "FCFS") {
         currentTime = endTime;
     }
 
-    // Draw the timeline
-    const drawTimeLine = document.getElementById("timeline");
-
-    for (let i = 1; i <= currentTime; i++) {
-        const div = document.createElement("div");
-        div.className = "milestone";
-        div.innerHTML = i;
-        div.style.width = `${unit}px`;
-        drawTimeLine.appendChild(div);
-    }
-
-    // Draw the Gantt chart
-    const ganttChart = document.getElementById("gantt-chart");
-    let gainCell = 0; // pill for flexbox's effect
-    for (let i = 0; i < timeline.length; i++) {
-        const entry = timeline[i];
-        const process = entry.process;
-        const startTime = entry.startTime - gainCell;
-        const endTime = entry.endTime - gainCell;
-        const width = (endTime - startTime) * unit;
-        const left = startTime * unit;
-        const color = `hsl(${i * unit}, 80%, 65%)`;
-        const div = document.createElement("div");
-
-        gainCell += endTime - startTime;
-
-        div.className = "process";
-        div.style.width = `${width}px`;
-        div.style.left = `${left}px`;
-        div.style.background = color;
-        div.dataset.name = process.name;
-        ganttChart.appendChild(div);
-    }
 }
 
 if (selectedAlg === "SJF_N") {
     heading.innerHTML = algorithms.SJF_N;
+
+    // Calculate for Gantt chart and timeline by SJF non-preemptive algorithm
+    let cloneProcesses = [...processes];
+    let currentProcesses = [];
+    let index = 0;
+    let countdown = processes.length;
+
+    while (countdown > 0) {
+        //console.log("cur",currentTime,...currentProcesses);
+        //console.log(...cloneProcesses);
+        // Get the current process
+        for (let i = 0; i < cloneProcesses.length; i++) {
+            const process = cloneProcesses[i];
+            if (process.arrivalTime <= currentTime) {
+                currentProcesses.push(process);
+                cloneProcesses.splice(i, 1);
+                i--;
+            }
+        }
+        //console.log("cur",currentTime,...currentProcesses);
+        //console.log(...cloneProcesses);
+
+        if (currentProcesses.length === 0) {currentTime = cloneProcesses[0].arrivalTime; continue;}
+        // Sort the current processes by burst time
+        currentProcesses.sort((a, b) => a.burstTime - b.burstTime);
+        const process = currentProcesses[0];
+        currentProcesses.splice(0, 1);
+        countdown--;
+        // Calculate the start and end times
+        // Start time is the current time
+        const endTime = currentTime + process.burstTime;
+
+        // Find the index of the process in the original array
+        index = processes.findIndex((p) => p.name === process.name);
+
+        timeline.push({ process: processes[index], startTime: currentTime, endTime: endTime });
+
+        // Calculate waiting time
+        processes[index].waitingTime = currentTime - processes[index].arrivalTime;
+        totalWaitingTime += processes[index].waitingTime;
+
+        // Calculate turn around time
+        processes[index].turnAroundTime = endTime - processes[index].arrivalTime;
+        totalTurnAroundTime += processes[index].turnAroundTime;
+        
+        // Calculate response time
+        processes[index].responseTime = currentTime - processes[index].arrivalTime;
+        totalResponseTime += processes[index].responseTime;
+
+        currentTime = endTime;
+    }
+
 }
 
 if (selectedAlg === "PRI_N") {
@@ -121,8 +140,6 @@ if (selectedAlg === "PRI_P") {
 if (selectedAlg === "RR") {
     heading.innerHTML = algorithms.RR;
 }
-
-console.log(timeline);
 
 // Print the processes
 const processList = document.getElementById("given-processes");
@@ -153,3 +170,38 @@ averageTurnAroundTime.innerHTML = (totalTurnAroundTime / processes.length).toFix
 // Calculate and print average response time
 const averageResponseTime = document.getElementById("avg-response-time-value");
 averageResponseTime.innerHTML = (totalResponseTime / processes.length).toFixed(2);
+
+// Draw the timeline
+const drawTimeLine = document.getElementById("timeline");
+for (let i = 1; i <= currentTime; i++) {
+    const div = document.createElement("div");
+    div.className = "milestone";
+    div.innerHTML = i;
+    div.style.width = `${unit}px`;
+    drawTimeLine.appendChild(div);
+}
+
+//console.log(...timeline);
+
+// Draw the Gantt chart
+const ganttChart = document.getElementById("gantt-chart");
+let gainCell = 0; // pill for flexbox's effect
+for (let i = 0; i < timeline.length; i++) {
+    const entry = timeline[i];
+    const process = entry.process;
+    const startTime = entry.startTime - gainCell;
+    const endTime = entry.endTime - gainCell;
+    const width = (endTime - startTime) * unit;
+    const left = startTime * unit;
+    const color = `hsl(${i * unit}, 80%, 65%)`;
+    const div = document.createElement("div");
+
+    gainCell += endTime - startTime;
+
+    div.className = "process";
+    div.style.width = `${width}px`;
+    div.style.left = `${left}px`;
+    div.style.background = color;
+    div.dataset.name = process.name;
+    ganttChart.appendChild(div);
+}
