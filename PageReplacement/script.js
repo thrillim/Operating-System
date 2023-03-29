95a2ec442c9b0023739c0255853e3af624ef3aca
thrillim <hauniuni23@gmail.com>
2023-03-29 16:34:02 +0700

    LRU, MRU, LFU, MFU, 2nd chance and add frame model

--- PageReplacement/script.js
+++ PageReplacement/script.js
@@ -8,6 +8,9 @@ let frames = parseInt(numOfFrames.value);
 const visualPageFrames = document.getElementById('visual-page-frames');
 const conclusion = document.getElementById('conclusion');
 
+const frameInfo = document.getElementById('frame-info');
+const frameSubInfo = document.getElementById('frame-sub-info');
+
 function visulize(refStringArray, frames, algorithm) {
   let pageFaults = 0;
   let isDrawn = false;
@@ -40,6 +43,124 @@ function visulize(refStringArray, frames, algorithm) {
         }
       }
       currentPageFrame = prevPageFrame;
+      frameSubInfo.innerHTML = "first-in index";
+    } else if (algorithm == 'LRU') {
+      prevPageFrame = currentPageFrame;
+      let indexOfRef = prevPageFrame.findIndex((item) => item[0] == refStringArray[i]);
+      if (indexOfRef == -1) { // Page Fault, not found
+        pageFaults++;
+        isDrawn = true;
+        if (!isFull) { // Page Frame is not full
+          prevPageFrame[prevPageFrame.findIndex((item) => item[0] == "-")] = [refStringArray[i], i];
+        } else { // Page Frame is full
+          // Find the least-recently used page frame
+          let leastRecentlyUsedFrameIndex = prevPageFrame.findIndex((item) => item[1] == Math.min(...prevPageFrame.map((item) => item[1])));
+          prevPageFrame[leastRecentlyUsedFrameIndex] = [refStringArray[i], i];
+        }
+      } else { // Page Hit, found
+        prevPageFrame[indexOfRef][1] = i;
+      }
+      currentPageFrame = prevPageFrame;
+      frameSubInfo.innerHTML = "least recent index";
+    } else if (algorithm == 'MRU') {
+      prevPageFrame = currentPageFrame;
+      let indexOfRef = prevPageFrame.findIndex((item) => item[0] == refStringArray[i]);
+      if (indexOfRef == -1) { // Page Fault, not found
+        pageFaults++;
+        isDrawn = true;
+        if (!isFull) { // Page Frame is not full
+          prevPageFrame[prevPageFrame.findIndex((item) => item[0] == "-")] = [refStringArray[i], i];
+        } else { // Page Frame is full
+          // Find the most-recently used page frame
+          let mostRecentlyUsedFrameIndex = prevPageFrame.findIndex((item) => item[1] == Math.max(...prevPageFrame.map((item) => item[1])));
+          prevPageFrame[mostRecentlyUsedFrameIndex] = [refStringArray[i], i];
+        }
+      } else { // Page Hit, found
+        prevPageFrame[indexOfRef][1] = i;
+      }
+      currentPageFrame = prevPageFrame;
+      frameSubInfo.innerHTML = "least recent index";
+    } else if (algorithm == 'LFU') {
+      prevPageFrame = currentPageFrame;
+      let indexOfRef = prevPageFrame.findIndex((item) => item[0] == refStringArray[i]);
+      if (indexOfRef == -1) { // Page Fault, not found
+        pageFaults++;
+        isDrawn = true;
+        if (!isFull) { // Page Frame is not full
+          prevPageFrame[prevPageFrame.findIndex((item) => item[0] == "-")] = [refStringArray[i], [1, i]]; // [frequency, first-in index]
+        } else { // Page Frame is full
+          // Find the least-frequently used page frame (if there are more than one, choose the first-in one)
+          let leastFrequentlyUsedFrameIndex;
+          // Find the least-frequently used page frames
+          let leastFrequentlyUsedFrames = prevPageFrame.filter((item) => item[1][0] == Math.min(...prevPageFrame.map((item) => item[1][0])));
+          // If there is only one least-frequently used page frame, choose it
+          if (leastFrequentlyUsedFrames.length == 1) {
+            leastFrequentlyUsedFrameIndex = prevPageFrame.findIndex((item) => item[0] == leastFrequentlyUsedFrames[0][0]);
+          } else { // If there are more than one least-frequently used page frames, choose the first-in one
+            leastFrequentlyUsedFrameIndex = prevPageFrame.findIndex((item) => item[1][1] == Math.min(...leastFrequentlyUsedFrames.map((item) => item[1][1])));
+          }
+          prevPageFrame[leastFrequentlyUsedFrameIndex] = [refStringArray[i], [1, i]];
+        }
+      } else { // Page Hit, found
+        prevPageFrame[indexOfRef][1][0]++;
+      }
+      currentPageFrame = prevPageFrame;
+      frameSubInfo.innerHTML = "frequency, first-in index";
+    } else if (algorithm == 'MFU') {
+      prevPageFrame = currentPageFrame;
+      let indexOfRef = prevPageFrame.findIndex((item) => item[0] == refStringArray[i]);
+      if (indexOfRef == -1) { // Page Fault, not found
+        pageFaults++;
+        isDrawn = true;
+        if (!isFull) { // Page Frame is not full
+          prevPageFrame[prevPageFrame.findIndex((item) => item[0] == "-")] = [refStringArray[i], [1, i]]; // [frequency, first-in index]
+        } else { // Page Frame is full
+          // Find the most-frequently used page frame (if there are more than one, choose the first-in one)
+          let mostFrequentlyUsedFrameIndex;
+          // Find the most-frequently used page frames
+          let mostFrequentlyUsedFrames = prevPageFrame.filter((item) => item[1][0] == Math.max(...prevPageFrame.map((item) => item[1][0])));
+          // If there is only one most-frequently used page frame, choose it
+          if (mostFrequentlyUsedFrames.length == 1) {
+            mostFrequentlyUsedFrameIndex = prevPageFrame.findIndex((item) => item[0] == mostFrequentlyUsedFrames[0][0]);
+          } else { // If there are more than one most-frequently used page frames, choose the first-in one
+            mostFrequentlyUsedFrameIndex = prevPageFrame.findIndex((item) => item[1][1] == Math.min(...mostFrequentlyUsedFrames.map((item) => item[1][1])));
+          }
+          prevPageFrame[mostFrequentlyUsedFrameIndex] = [refStringArray[i], [1, i]];
+        }
+      } else { // Page Hit, found
+        prevPageFrame[indexOfRef][1][0]++;
+      }
+      currentPageFrame = prevPageFrame;
+      frameSubInfo.innerHTML = "frequency, first-in index";
+    } else if (algorithm == '2ND') {
+      prevPageFrame = currentPageFrame;
+      let indexOfRef = prevPageFrame.findIndex((item) => item[0] == refStringArray[i]);
+      let refPointer = 0;
+      if (indexOfRef == -1) { // Page Fault, not found
+        pageFaults++;
+        isDrawn = true;
+        if (!isFull) { // Page Frame is not full
+          prevPageFrame[prevPageFrame.findIndex((item) => item[0] == "-")] = [refStringArray[i], [0, i]]; // refference bit, first-in index
+        } else { // Page Frame is full
+          // Find the oldest page frame as the first refPointer
+          refPointer = prevPageFrame.findIndex((item) => item[1][1] == Math.min(...prevPageFrame.map((item) => item[1][1])));
+          while (prevPageFrame[refPointer][1][0] == 1) { // If refference bit is 1 then change it to 0
+            prevPageFrame[refPointer][1][0] = 0;
+            refPointer = (refPointer + 1) % frames;
+          }
+          // If refference bit is 0 then replace it
+          prevPageFrame[refPointer] = [refStringArray[i], [0, i]];
+        }
+      } else { // Page Hit, found
+        // If refference bit is 0 then change it to 1
+        if (prevPageFrame[indexOfRef][1][0] == 0) {
+          prevPageFrame[indexOfRef][1][0] = 1;
+        }
+        // If refference bit is 1 then do nothing
+      }
+      currentPageFrame = prevPageFrame
+      frameSubInfo.innerHTML = "reference bit, first-in index";
+    } else if (algorithm == 'OPT') {
     }
     // Draw current page frame
     let pageFrame = "";
@@ -58,6 +179,8 @@ function visulize(refStringArray, frames, algorithm) {
   }
   // Show conclusion
   conclusion.innerHTML = `Page Faults: ${pageFaults}`;
+  // Display frame info
+  frameInfo.className = "border-solid border border-primary text-center max-w-fit m-auto px-2";
 }
 
 algorithm.addEventListener('change', () => {
